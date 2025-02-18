fd6324847a15c12a1b045da87d06a8692e607534
ChrisLuckComes <313963196@qq.com>
2025-02-18 15:51:57 +0800

    Updates

---
+++ assets/Anchor-8814bbd4.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-aeb01ca2.js";import{A as r}from"./index-5e35097d.js";const a=({items:t})=>o.jsx(r,{className:"h-full overflow-y-auto",getContainer:()=>document.querySelector(".article"),items:t,targetOffset:0});export{a as A};
--- assets/Anchor-b9aeacb8.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./index-a433b440.js";import{A as r}from"./index-b35002ab.js";const a=({items:t})=>o.jsx(r,{className:"h-full overflow-y-auto",getContainer:()=>document.querySelector(".article"),items:t,targetOffset:0});export{a as A};

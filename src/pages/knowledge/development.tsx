50877f1b9b78305b6512386d9402ad40f3db0466
luoyunlai <313963196@qq.com>
2025-03-04 17:42:12 +0800

    feature: 更新MVC模式

--- src/pages/knowledge/development.tsx
+++ src/pages/knowledge/development.tsx
@@ -1,8 +1,8 @@
-import { classMap } from "@/constants/constant";
+import { classMap } from '@/constants/constant';
 
-import DEVELOPMENT from "@images/knowledge/development.png";
-import { LazyImage } from "@/component/image";
-import { ArticleAnchor } from "@/component/Anchor";
+import DEVELOPMENT from '@images/knowledge/development.png';
+import { LazyImage } from '@/component/image';
+import { ArticleAnchor } from '@/component/Anchor';
 
 export default function Index() {
   return (
@@ -20,12 +20,39 @@ export default function Index() {
         <h3 id="mvc" className={classMap.articleSubTitle}>
           MVC(Model View Controller)
         </h3>
-        MVC改变了命令式的代码组织，但它的缺点是代码组织起来较为复杂，首先需要定义model，然后需要view中定义属性变化后的dom修改函数。
+        MVC是一种软件架构模式，将应用程序分为三个主要部分，模型（Model）、视图（View）和控制器（Controller），它改变了命令式的代码组织方式。用户通过视图触发操作，控制器接收操作并调用模型的方法进行处理，模型处理完数据后将结果返回给控制器，
+        控制器再根据结果更新视图。
+        <ul className={classMap.ul}>
+          <li>
+            <strong>模型（Model）:</strong>
+            负责处理应用程序的数据和业务逻辑，与数据库或其他数据存储进行交互，通常用于存储和管理数据对象，以及执行数据操作和验证等任务。
+          </li>
+          <li>
+            <strong>视图（View）:</strong>
+            主要用于展示数据给用户，它是用户和应用程序交互的界面部分，通常由HTML、CSS、JavaScript等技术实现，负责将模型中的数据以特定的形式呈现给用户
+          </li>
+          <li>
+            <strong>控制器（Controller）:</strong>
+            作为中间桥梁，接受用户的输入和操作，根据请求调用相应的模型方法来处理数据，并根据处理结果选择合适的视图来展示给用户，起到了协调模型和试图的作用。
+          </li>
+        </ul>
         <h3 id="mvvm" className={classMap.articleSubTitle}>
           MVVM(Model View ViewModel)
         </h3>
-        它和MVC最大的区别就是，通过双向绑定等方式实现了数据变动后自动渲染视图，屏蔽了dom操作，无需开发者手动修改。
-        简化了视图和数据的依赖，带来了虚拟dom的理念，解决了频繁更新的问题。
+        它由MVC架构演变而来，<code>Controller</code>变为<code>ViewModel</code>
+        ，视图通过数据绑定与ViewModel建立关系，ViewModel监听视图的变化并更新模型，同时模型的变化也会通过ViewModel自动更新到视图上，实现了数据的双向绑定。
+        <strong>和MVC的区别</strong>
+        <br />
+        <ul className={classMap.ul}>
+          <strong>数据绑定方式：</strong>
+          <br />
+          <li>
+            MVC需要在控制器中手动更新视图，数据流向相对单向。MVVM的数据变化自动同步，大幅减少了手动操作DOM更新视图的代码，提高了开发效率和可维护性。
+          </li>
+          <li>
+            MVC中Controller职责较重，需要处理大量的业务逻辑、视图和模型之间的交互逻辑，导致代码复杂度过高。MVVM中ViewModel承担了更多与视图相关的数据转换工作，使得视图和模型之间的职责边界更加清晰，视图只关注展示，模型只关注数据和业务逻辑。
+          </li>
+        </ul>
         <h3 id="node" className={classMap.articleSubTitle}>
           Node.js
         </h3>
@@ -66,44 +93,44 @@ export default function Index() {
       <ArticleAnchor
         items={[
           {
-            title: "前端技术发展轨迹",
-            key: "pre",
-            href: "#pre",
+            title: '前端技术发展轨迹',
+            key: 'pre',
+            href: '#pre',
             children: [
               {
-                title: "ajax",
-                key: "ajax",
-                href: "#ajax"
+                title: 'ajax',
+                key: 'ajax',
+                href: '#ajax'
               },
               {
-                title: "MVC",
-                key: "mvc",
-                href: "#mvc"
+                title: 'MVC',
+                key: 'mvc',
+                href: '#mvc'
               },
               {
-                title: "MVVM",
-                key: "mvvm",
-                href: "#mvvm"
+                title: 'MVVM',
+                key: 'mvvm',
+                href: '#mvvm'
               },
               {
-                title: "Node.js",
-                key: "node",
-                href: "#node"
+                title: 'Node.js',
+                key: 'node',
+                href: '#node'
               },
               {
-                title: "Serverless",
-                key: "serverless",
-                href: "#serverless"
+                title: 'Serverless',
+                key: 'serverless',
+                href: '#serverless'
               },
               {
-                title: "技术方案",
-                key: "plan",
-                href: "#plan"
+                title: '技术方案',
+                key: 'plan',
+                href: '#plan'
               },
               {
-                title: "渲染方案",
-                key: "render",
-                href: "#render"
+                title: '渲染方案',
+                key: 'render',
+                href: '#render'
               }
             ]
           }
